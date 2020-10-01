e7b71c8493d89c83dab8958d7694e222595293f0
Ilona Dvorak <ilonab.dvorak@gmail.com>
2020-10-01 17:06:44 +0200

    print name exercise test 1

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -5,4 +5,11 @@ require_relative "compute_name"
 # TODO: ask for the last name
 # TODO: Print a greeting to the user with the full name concatenated,
 #       something like "Hello, Boris Paillard"
+puts "Whats your first name ?"
+first_name = gets.chomp
+puts "Whats your middle name ?"
+middle_name = gets.chomp
+puts " Whats your last name ?"
+last_name =  gets.chomp
 
+puts "Hello, #{first_name} #{middle_name} #{last_name}"
