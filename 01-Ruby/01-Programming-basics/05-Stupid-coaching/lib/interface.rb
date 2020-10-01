00b540944bd7476057a64a8ec58626b369bed049
Ilona Dvorak <ilonab.dvorak@gmail.com>
2020-10-01 20:05:18 +0200

    exercise 5

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,5 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+puts "Nice day today"
+message = gets.chomp
+puts coach_answer_enhanced(message)
