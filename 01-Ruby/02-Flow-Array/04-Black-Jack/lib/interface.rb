03f14ecf4511f75afcefd397de99bba270f88413
Ilona Dvorak <ilonab.dvorak@gmail.com>
2020-10-03 18:23:03 +0200

    test 1

--- 01-Ruby/02-Flow-Array/04-Black-Jack/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/04-Black-Jack/lib/interface.rb
@@ -1,4 +1,10 @@
 require_relative "croupier"
-
 # TODO: make the user play from terminal in a while loop that will stop
 #       when the user will not be asking for  a new card
+while players_answer != "no"
+  puts "Card? 'y' or 'yes' to get a new card"
+  players_answer = gets.chomp
+  if players_answer.downcase == "y" || players_answer.downcase == "yes"
+    puts "You beat the bank! You win."
+  end
+end
