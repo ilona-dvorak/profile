b3c8840f6446b0d5c02293cc4331d1c7317c611a
Ilona Dvorak <ilonab.dvorak@gmail.com>
2020-10-03 14:30:46 +0200

    too long code but will be corrected later

--- 01-Ruby/02-Flow-Array/02-Simple-looping/lib/simple_looping.rb
+++ 01-Ruby/02-Flow-Array/02-Simple-looping/lib/simple_looping.rb
@@ -1,7 +1,28 @@
 def sum_with_for(min, max)
+  sum = 0
   # CONSTRAINT: you should use a for..end structure
+  for num in (min..max)
+    sum = sum + num
+  end
+  if min > max
+    return -1
+  else
+    return sum
+  end
 end
 
+puts sum_with_for(1, 5)
 def sum_with_while(min, max)
   # CONSTRAINT: you should use a while..end structure
+  counter = min
+  sum = 0
+  while counter <= max
+    sum += counter
+    counter += 1
+  end
+  if min > max
+    return -1
+  else
+    return sum
+  end
 end
