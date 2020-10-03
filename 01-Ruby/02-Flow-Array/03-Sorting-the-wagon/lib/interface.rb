88cd91e85feb714fbe0e70970f868c551e5fa301
Ilona Dvorak <ilonab.dvorak@gmail.com>
2020-10-03 15:31:42 +0200

    test 1

--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
@@ -4,8 +4,19 @@ require_relative "wagon_sort"
 #       Remember, to read an input from the command line, use:
 #       - `gets`:  http://www.ruby-doc.org/core-2.5.3/Kernel.html#method-i-gets
 #       - `chomp`: http://www.ruby-doc.org/core-2.5.3/String.html#method-i-chomp
-
-
-
+puts "Type a student name: "
+student_name = gets.chomp
+student_list = []
+student_list << student_name
+while student_name != ""
+  puts "Type another student name or press enter to finish: "
+  student_name = gets.chomp
+  student_list << student_name
+end
+student_list.delete("")
+puts "Congratulations! Your Wagon has #{student_list.length} students:"
 # TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
 #       file and display the sorted student list
+student_sorted = wagon_sort(student_list)
+
+puts "#{student_sorted[0..-2].join(', ')} and #{student_sorted[-1]} "
