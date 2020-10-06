dc7e652039a67d38ed7ed16a2dbb281813892dcc
Ilona Dvorak <ilonab.dvorak@gmail.com>
2020-10-06 21:44:06 +0200

    test 3

--- 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/burger_restaurant.rb
+++ 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/burger_restaurant.rb
@@ -1,3 +1,8 @@
 def burger(patty, sauce, topping)
-  # TODO: code the `burger` method
+  burger_components = ["bread", patty, sauce, topping, "bread"]
+  if block_given?
+    on_demand_patty = yield(patty)
+    burger_components = ["bread", on_demand_patty, sauce, topping, "bread"]
+  end
+  return burger_components
 end
--- 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/interface.rb
+++ 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/interface.rb
@@ -1,20 +1,26 @@
 require_relative "burger_restaurant"
-
-puts "💬 I'd like a burger with a bigger portion of fish, plus mayo and salad please."
+puts ":speech_balloon: I'd like a burger with a bigger portion of fish, plus mayo and salad please."
 # TODO: to upgrade a portion to a bigger one, transform the fish to uppercase
-# bigger_burger =
-
-
-puts "💬 I'd like a juicy steak with barbecue sauce and cheddar please."
+bigger_burger = burger("fish", "mayo", "salad") do |patty|
+  patty.upcase
+end
+puts ":speech_balloon: I'd like a juicy steak with barbecue sauce and cheddar please."
 # TODO: to make a juicy steak, replace any vowel by the sign "~"
-# juicy_burger =
-
-
-puts "💬 I'd like a spicy chicken with ketchup and cheddar please."
+juicy_burger = burger("steak", "barbecue", "cheddar") do |patty|
+  patty.gsub(/[aeiou]/, "~")
+end
+puts ":speech_balloon: I'd like a spicy chicken with ketchup and cheddar please."
 # TODO: to make a spicy portion, add the sign "*" before and after the string
-# spicy_burger =
-
-
+spicy_burger = burger("chicken", "ketchup", "cheddar") do |patty|
+  "*#{patty}*"
+end
+grilles_burger = burger("steak", "ketchup", "onions") do |patty|
+  "grilled #{patty}"
+end
+puts grilles_burger
+puts bigger_burger
+puts spicy_burger
+puts bigger_burger
 
 
 
