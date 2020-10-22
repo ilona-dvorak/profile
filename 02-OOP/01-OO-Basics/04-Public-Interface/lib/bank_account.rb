56145be6dfb1074237cc472e00e5d1f64fce6f26
Ilona Dvorak <ilonab.dvorak@gmail.com>
2020-10-22 20:46:33 +0200

    test

--- 02-OOP/01-OO-Basics/04-Public-Interface/lib/bank_account.rb
+++ 02-OOP/01-OO-Basics/04-Public-Interface/lib/bank_account.rb
@@ -10,6 +10,7 @@ class BankAccount
   # - you can print transactions only with a password
   # - you can withdraw or deposit money
   # - You can see the balance of the account (through the balance variable)
+  attr_reader :name, :balance
 
   MIN_DEPOSIT = 100
 
@@ -28,25 +29,39 @@ class BankAccount
   def withdraw(amount)
     # TODO: Call add_transaction with the right argument
     # TODO: returns a string with a message
+    withdrawn_amount = "-#{amount}".to_i
+    add_transaction(withdrawn_amount)
+    "You've just withdrawn/deposited #{amount} euros"
   end
 
   def deposit(amount)
     # TODO: Call add_transaction with the right argument
     # TODO: returns a string with a message
+    add_transaction(amount)
+    "You've withdrawn/deposited #{amount} euros"
   end
 
   def transactions_history(args = {})
     # TODO: Check if there is a password and if so if it is correct
     # TODO: return a string displaying the transactions, BUT NOT return the transaction array!
+    if args[:password] == @password
+      @transactions.join("\n")
+    elsif  args.empty?
+      "no password given"
+    else
+      "wrong password"
+    end
   end
 
   def iban
     # TODO: Hide the middle of the IBAN like FR14**************606 and return it
+    @iban.gsub(/(-)(\w{4})?.{5}/, "*")
   end
 
   def to_s
     # Method used when printing account object as string (also used for string interpolation)
     # TODO: return a string with the account owner, the hidden iban and the balance of the account
+    "Owner: #{@name}, IBAN: #{iban}, Balance: #{@balance}"
   end
 
   private
@@ -54,5 +69,7 @@ class BankAccount
   def add_transaction(amount)
     # TODO: add the amount in the transactions array
     # TODO: update the current balance (which represents the balance of the account)
+    @transactions << amount
+    @balance += amount
   end
 end
