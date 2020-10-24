af39716c79aa711adf4c6a7f2abdc26a37f7a5aa
Ilona Dvorak <ilonab.dvorak@gmail.com>
2020-10-24 12:50:06 +0200

    test 1

--- 02-OOP/02-OO-Advanced/03-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/03-Instance-vs-Class/lib/restaurant.rb
@@ -1,9 +1,22 @@
 class Restaurant
   # TODO: add relevant accessors if necessary
+  attr_reader :average_rating, :city
 
   def initialize(city, name)
     # TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @ratings = []
+    @average_rating = 0
+  end
+  # TODO: implement .filter_by_city and #rate methods
+
+  def rate(new_rate)
+    @ratings << new_rate
+    @average_rating = @ratings.reduce(:+).fdiv(@ratings.length)
   end
 
-  # TODO: implement .filter_by_city and #rate methods
+  def self.filter_by_city(restaurants, city)
+    restaurants.filter { |restaurant| restaurant.city == city }
+  end
 end
