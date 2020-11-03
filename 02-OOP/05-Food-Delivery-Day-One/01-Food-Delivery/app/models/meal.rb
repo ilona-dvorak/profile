9bb12ce0ea140887cc82c09bbff303a0a4441ba5
Ilona Dvorak <ilonab.dvorak@gmail.com>
2020-11-03 19:18:02 +0100

    meal only

---
+++ 02-OOP/05-Food-Delivery-Day-One/01-Food-Delivery/app/models/meal.rb
@@ -0,0 +1,11 @@
+class Meal
+  attr_reader :price, :name
+  attr_accessor :id
+
+
+  def initialize(attributes = {})
+    @id = attributes[:id]
+    @name = attributes[:name]
+    @price = attributes[:price]
+  end
+end
