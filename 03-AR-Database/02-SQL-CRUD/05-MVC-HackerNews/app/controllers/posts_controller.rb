f15182dfb956e3a81800f2e6856c44c0c689482e
Ilona Dvorak <ilonab.dvorak@gmail.com>
2020-11-14 16:50:28 +0100

    parts

--- 03-AR-Database/02-SQL-CRUD/05-MVC-HackerNews/app/controllers/posts_controller.rb
+++ 03-AR-Database/02-SQL-CRUD/05-MVC-HackerNews/app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
-require_relative '../models/post.rb'
-require_relative '../views/posts_view.rb'
+require 'byebug'
+require_relative '../models/post'
+require_relative '../views/posts_view'
 
 class PostsController
   def initialize
@@ -11,65 +12,40 @@ class PostsController
   ################################################################
 
   def index
-    # TODO: implement listing all posts
-    posts = Post.all
-    @view.display_posts(posts)
+    post = Post.all
+    @view.list(post)
   end
 
   def create
-    # TODO: implement creating a new post
-    title = @view.ask_for("title")
-    url = @view.ask_for("url")
-    post = Post.new
-    post.title = title
-    post.url = url
-    post.save
-    @view.show_message("New Post created!!")
-
+    post_info = @view.add_to_db
+    new_post = Post.new(post_info)
+    new_post.save
   end
 
   def update
-    # TODO: implement updating an existing post
-    post_id = @view.ask_for("id")
-    post = Post.find(post_id.to_i)
-    post.nil? ? update : update_post(post)
+    index
+    id_info = @view.update_the_db
+    find_id = Post.find(id_info[:id])
+    title_url_info = @view.add_to_db
+    full_info = { "id" => id_info[:id].to_i,
+                  "title" => title_url_info[:title],
+                  "url" => title_url_info[:url] }
+    updated_post = Post.build_record(full_info)
+    updated_post.save
   end
 
   def destroy
-    # TODO: implement destroying a post
-    post_id = @view.ask_for("id")
-    post = Post.find(post_id.to_i)
-    post.nil? ? destroy : destroy_post(post)
+    index
+    id_info = @view.destroy_from_db
+    find_id = Post.find(id_info[:id])
+    find_id.destroy
   end
 
   def upvote
-    # TODO: implement upvoting a post
     index
-    post_id = @view.ask_for("id")
-    post = Post.find(post_id.to_i)
-    post.nil? ? upvote : upvote_post(post)
-  end
-
-  private
-
-
-  def update_post(post)
-    title = @view.ask_for("new title")
-    url = @view.ask_for("new url")
-    post.title = title
-    post.url = url
-    post.save
-    @view.show_message("Post ID #{post.id} updated!!")
-  end
-
-  def destroy_post(post)
-    post.destroy
-    @view.show_message("Post ID #{post.id} destroyed!!")
-  end
-
-  def upvote_post(post)
-    post.upvote!
-    post.save
-    @view.show_message("Post ID #{post.id} upvoted!!")
+    id_info = @view.upvote
+    updated_post = Post.find(id_info[:id])
+    updated_post.votes += 1
+    updated_post.save
   end
 end
