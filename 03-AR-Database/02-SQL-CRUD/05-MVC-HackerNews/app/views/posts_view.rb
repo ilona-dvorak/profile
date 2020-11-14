ea4771daae459babc3b0f15a812d8e09f01d696f
Ilona Dvorak <ilonab.dvorak@gmail.com>
2020-11-14 15:46:34 +0100

    parts of the exercise

--- 03-AR-Database/02-SQL-CRUD/05-MVC-HackerNews/app/views/posts_view.rb
+++ 03-AR-Database/02-SQL-CRUD/05-MVC-HackerNews/app/views/posts_view.rb
@@ -1,3 +1,21 @@
 class PostsView
   # TODO: implement methods called by the PostsController
+  def display_posts(posts)
+    puts ""
+    puts "All Posts"
+    posts.each do |post|
+      puts "Post ID: #{post.id} - #{post.title} - #{post.url} - Votes: #{post.votes}"
+    end
+    puts ""
+  end
+
+  def ask_for(item)
+    puts "What is the #{item} of the post:"
+    print "> "
+    gets.chomp
+  end
+
+  def show_message(message)
+    puts "\n#{message}\n"
+  end
 end
