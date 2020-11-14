411709b21d43ba15cae663ddc1bdf008c536c2b5
Ilona Dvorak <ilonab.dvorak@gmail.com>
2020-11-14 12:13:28 +0100

    test 4

--- 03-AR-Database/02-SQL-CRUD/01-Query-the-DB/lib/query.rb
+++ 03-AR-Database/02-SQL-CRUD/01-Query-the-DB/lib/query.rb
@@ -1,5 +1,5 @@
 require 'sqlite3'
-#comment for repush
+
 def all_artists(db)
   # TODO: use `db` to execute an SQL query against the database to
   # retrieve all columns from all records from the `artists` table.
