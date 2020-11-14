630abe562ae20f383b9969d76b05d01eb969ca86
Ilona Dvorak <ilonab.dvorak@gmail.com>
2020-11-14 16:49:52 +0100

    parts of it

--- 03-AR-Database/02-SQL-CRUD/05-MVC-HackerNews/app/views/posts_view.rb
+++ 03-AR-Database/02-SQL-CRUD/05-MVC-HackerNews/app/views/posts_view.rb
@@ -1,21 +1,38 @@
 class PostsView
   # TODO: implement methods called by the PostsController
-  def display_posts(posts)
-    puts ""
-    puts "All Posts"
+  def list(posts)
     posts.each do |post|
-      puts "Post ID: #{post.id} - #{post.title} - #{post.url} - Votes: #{post.votes}"
+      puts "ID: #{post.id} NAME: #{post.title} URL: #{post.url} VOTES: #{post.votes}"
     end
-    puts ""
   end
 
-  def ask_for(item)
-    puts "What is the #{item} of the post:"
-    print "> "
-    gets.chomp
+  def add_to_db
+    puts 'Please give us the name of the post'
+    print '> '
+    answer_one = gets.chomp
+    puts 'Please give us the URL of the post'
+    print '> '
+    answer_two = gets.chomp
+    { title: answer_one, url: answer_two }
   end
 
-  def show_message(message)
-    puts "\n#{message}\n"
+  def update_the_db
+    puts "This is all our available posts"
+    puts "Please tell us the ID so we can update without fuckups"
+    answer_three = gets.chomp
+    { id: answer_three }
+  end
+
+  def destroy_from_db
+    puts "This is all our available posts"
+    puts "Which mofo you wanna DESTROY FOR EVAH?!? EVAH? EVAH EVAH? - Kayne West"
+    answer_four = gets.chomp
+    { id: answer_four }
+  end
+
+  def upvote
+    puts "Which post you wanna shill so badly?"
+    answer_five = gets.chomp
+    { id: answer_five }
   end
 end
