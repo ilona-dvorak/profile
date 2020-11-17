26dc5f16a78eed69ca425d1cbff2ed5b5f0e2341
Ilona Dvorak <ilonab.dvorak@gmail.com>
2020-11-17 20:10:26 +0100

    test4

--- 03-AR-Database/03-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
@@ -1,2 +1,6 @@
 require "faker"
-# TODO: Write a seed to insert 100 posts in the database
+100.times do
+  Post.create(title: Faker::Hacker.say_something_smart,
+              url: Faker::Internet.url,
+              votes: Faker::Number.number(digits: 4))
+end
