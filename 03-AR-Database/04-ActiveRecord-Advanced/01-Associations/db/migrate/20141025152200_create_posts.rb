38fd552a2fdf15e1821e3bd0b2e2ae5ab4165ba3
Ilona Dvorak <ilonab.dvorak@gmail.com>
2020-11-21 12:15:58 +0100

    test 3

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/migrate/20141025152200_create_posts.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/migrate/20141025152200_create_posts.rb
@@ -5,6 +5,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
       t.string :url
       t.integer :votes, default: 0
       t.timestamps null: false
+      t.references :user, foregin_key: true, null: false
     end
   end
 end
