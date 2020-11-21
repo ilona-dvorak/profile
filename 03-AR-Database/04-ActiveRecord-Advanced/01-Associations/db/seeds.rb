4c2eece5c5b4f981f195126594ab4a609d4e3e83
Ilona Dvorak <ilonab.dvorak@gmail.com>
2020-11-21 12:15:10 +0100

    test 1

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,1 +1,21 @@
 # TODO: Write a seed
+require 'faker'
+
+puts "creating 5 fake users '"
+5.times do
+  user = User.new(
+    username: Faker::Name.name,
+    email: Faker::Internet.email
+  )
+  user.save!
+
+  rand(5..10).times do
+    post = Post.new(
+      title: Faker::Name.name,
+      url: Faker::Name.name,
+      votes: rand(0..5),
+      user: user
+    )
+    post.save!
+  end
+end
